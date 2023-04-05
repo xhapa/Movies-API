629c7f23680959eee2fe6c63dbe672f1f3f14cd7
Xhapa <emicb0111@gmail.com>
2023-04-05 11:28:17 -0500

    Modulation refactor

--- main.py
+++ main.py
@@ -1,94 +1,22 @@
 #Python
 from typing import Optional, List
 
-#Pydantic
-from pydantic import BaseModel
-from pydantic import Field
-
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Request, Depends
+from fastapi import Body, Query, Path, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.security import HTTPBearer
-from fastapi.exceptions import HTTPException
 
 #Own
-from jwt_manager import create_token, validate_token
+from jwt_manager import create_token
+from models import Movie, User
+from security import JWTBearer
 
+# Movies App -------------------------------------------------------------------
 app = FastAPI()
 app.title = "My app"
 app.version = "0.0.2"
 
 movies = []
-
-# Security
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if data['email'] != 'admin@gmail.com':
-            raise HTTPException(status_code=403, detail="Credentials are invalid")
-#Models
-class Movie(BaseModel):
-    id: int = Field(
-        ...,
-        gt=0,
-        title="Movie ID",
-        description="This is the movie ID"
-    )
-    title: str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        title="Movie title",
-        description="This is the movie title"
-    )
-    overview: str = Field(
-        ...,
-        min_length=1,
-        max_length=400,
-        title="Movie Overview",
-        description="This is the movie overview"
-    )
-    year: int = Field(
-        ...,
-        gt=0,
-        le=2023,
-        title="Movie year",
-        description="This is the movie year"
-    )
-    rating: Optional[float] = Field(
-        default=None,
-        ge=0,
-        le=10,
-        title="Movie rating",
-        description="This is the movie rating"
-    )
-    category: str = Field(
-        ...,
-        max_length=50,
-        min_length=1,
-        title="Movie category",
-        description="This is the movie category"
-    )
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 1,
-                "title": 'Ant-Man and the Wasp: Quantumania',
-                "overview": "Ant-Man and the Wasp find themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
-                "year": 2023,
-                "rating": 9.2,
-                "category": "Adventure/Action"
-            }
-        }
-
-class User(BaseModel):
-    email: str = Field(...)
-    password: str = Field(...)
-
-# Movies App -------------------------------------------------------------------
 
 # Home page
 @app.get('/')
---
+++ security.py
@@ -0,0 +1,15 @@
+# FastAPI
+from fastapi.security import HTTPBearer
+from fastapi.exceptions import HTTPException
+from fastapi import Request
+
+#Own
+from jwt_manager import validate_token
+
+# Security
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth = await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != 'admin@gmail.com':
+            raise HTTPException(status_code=403, detail="Credentials are invalid")
